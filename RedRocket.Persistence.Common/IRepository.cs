cda317caa20cd15927473de07e48e7a3c10bfd6f
Tyler Garlick <tjgarlick@gmail.com>
2013-06-11 08:56:22 -0400

    after merge conflict

---
+++ RedRocket.Persistence.Common/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using RedRocket.Utilities.Core.Validation;
+
+namespace RedRocket.Persistence.Common
+{
+    public partial interface IRepository<T> : IReadOnlyRepository<T> where T : class
+    {
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(T entity);
+        
+        IEnumerable<ObjectValidationError> Validate(T entity);
+    }
+}
